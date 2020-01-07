a4705a94f9bd675dd6eb1d5c775bf072b5b2e78e
BuildTools <wmstudt25@gmail.com>
2020-01-07 10:28:46 -0600

    Git test...again

--- build/CMakeFiles/venus.dir/DependInfo.cmake
+++ build/CMakeFiles/venus.dir/DependInfo.cmake
@@ -1,7 +1,17 @@
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
   )
 # The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/wesley/Workspace/venus/src/vwindow.cpp" "/home/wesley/Workspace/venus/build/CMakeFiles/venus.dir/src/vwindow.cpp.o"
+  "/home/wesley/Workspace/venus/test.cpp" "/home/wesley/Workspace/venus/build/CMakeFiles/venus.dir/test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
--- build/CMakeFiles/venus.dir/cmake_clean.cmake
+++ build/CMakeFiles/venus.dir/cmake_clean.cmake
@@ -1,5 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/venus.dir/test.cpp.o"
+  "CMakeFiles/venus.dir/src/vwindow.cpp.o"
+  "venus.pdb"
+  "venus"
+)
 
 # Per-language clean rules from dependency scanning.
-foreach(lang )
+foreach(lang CXX)
   include(CMakeFiles/venus.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
